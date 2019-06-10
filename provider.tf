7f652ba12295230e82458eba9800780f1bea5aa5
SAPIENT\ash195 <asharma197@sapient.com>
2019-06-10 23:33:58 +0530

    key

--- provider.tf
+++ provider.tf
@@ -1,7 +1,6 @@
 provider "aws" {
-  access_key = "AKIAQ7PI7TW6MN6FQNUO"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region     = "${var.region}"
 
 }
-
