cfaeccb13dc7c140c4ee9e20d16b698c9d105db7
root <root@ip-172-31-86-118.ec2.internal>
2019-06-12 17:09:58 +0000

    ELB SETUP WITH IN PUBLIC VPC

--- elb.tf
+++ elb.tf
@@ -1,8 +1,8 @@
 # Create a new load balancer
 resource "aws_elb" "myelb" {
   name               = "my-terraform-elb"
-  availability_zones = ["${var.az}"]
-  subnets            = ["${aws_subnet.public.*.id}"]
+  #availability_zones = ["${var.az}"]
+  subnets            = "${aws_subnet.public.*.id}"
   security_groups    = ["${aws_security_group.webserver.id}"]
 
 
@@ -22,7 +22,7 @@ resource "aws_elb" "myelb" {
     interval            = 30
   }
 
-  instances                   = ["${aws_instance.webservers.*.id}"]
+  instances                   = "${aws_instance.webservers.*.id}"
   cross_zone_load_balancing   = true
   idle_timeout                = 100
   connection_draining         = true
